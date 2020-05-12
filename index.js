82027a6fa204cdeb627b4dd03eeb55d5448c2c47
Ting Chung <tchung682@gmail.com>
2020-05-12 21:26:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function takeANumber(current,newPerson){
   var number = current.length
   return "Welcome, " + newPerson + ". You are number " + number + " in line."
 }
+function nowServing(current){
+  var number = current.length
+  if (current.length != 0){
+    return current.shift();
+
+    else {
+        return "There is nobody waiting to be served!"
+    }
+  }
+  
+}
