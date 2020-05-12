197e61667e40f7130eb9474f0a9a1ebe14cfc2ae
Ting Chung <tchung682@gmail.com>
2020-05-12 21:22:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function takeANumber(current,newPerson){
+
   current[current.length] = newPerson
-  number = current.length
+  var number = current.length
   return "Welcome " + newPerson + ". You are number " + number + " in line."
 }
